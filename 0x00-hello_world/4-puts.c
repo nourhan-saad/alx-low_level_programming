8287ef6a0abe577535421b308742984f03d3a7eb
nourhan-saad <nourhansaad707@gmail.com>
2023-08-11 17:02:20 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Discription: A C program that prints with put function
+ * Description: A C program that prints with put function
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle:");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
