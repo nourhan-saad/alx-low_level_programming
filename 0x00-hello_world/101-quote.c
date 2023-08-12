6d51bf32256b6d0d9809234d8ec1acc2f2ba1016
nourhan-saad <nourhansaad707@gmail.com>
2023-08-11 18:39:25 -0700

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ *main - entry point
+ *
+ *Description: print a quate usint write function
+ *writ (int fd.const void buf.size_t count)
+ *
+ *return: 1 (notsucces)
+*/
+int main(void)
+{
+	char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+
+	return (1);
+}
