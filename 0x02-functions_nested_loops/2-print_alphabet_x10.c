55fe7c7e22a57d752cfc57133373f8228e002df4
nourhan-saad <nourhansaad707@gmail.com>
2023-08-15 18:14:07 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 is needed to print the alphabet ten times
+ *in lower cass so we need to make two loops inside each other
+ *and no return due to exsestace of void .
+*/
+void print_alphabet_x10(void)
+{
+	int ch;
+	int sh;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putcher(ch);
+		for (sh = 0; sh <= 9; sh++)
+			_putcher(sh);
+}
