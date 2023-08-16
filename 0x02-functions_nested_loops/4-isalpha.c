38482457ef4e9040b3fc77f805ac72fc5af5dc5e
nourhan-saad <nourhansaad707@gmail.com>
2023-08-15 19:16:04 -0700

    s

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isalpha - checks if the character is a later
+ *			both lowercase or upercase
+ *
+ * @c: takes input from other function
+ *
+ *Return: 1 if c is true else 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
