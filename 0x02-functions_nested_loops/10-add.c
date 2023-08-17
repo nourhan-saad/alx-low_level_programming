4d5f0180ae4a1ff4531ebc17ab29b7e12607627a
nourhan-saad <nourhansaad707@gmail.com>
2023-08-16 19:07:06 -0700

    s

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *add  - add two integers
+ *
+ * int - is a parameter to ponint its an integer number
+ *
+ *@a: first input
+ *@b: secound input
+ *
+ * Return: sum of a & b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
