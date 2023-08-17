e368cd4ecb499ad3ff543af30adf1e997a6f803c
nourhan-saad <nourhansaad707@gmail.com>
2023-08-16 20:36:56 -0700

    s

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entre point
+ *
+ * Descraption: complete the sum of all
+ *		the multeplays of 3 or 5
+ *		bellow 1024 (excuted)
+ *
+ * Return: 0 always succsess
+*/
+int main(void)
+{
+	int num, sum;
+
+	for (num = 0; num <= 1024; num++)
+	{
+		if ((num % 3) || (num % 5))
+			sum += num;
+	}
+	printf("%d/n", sum);
+
+	return (0);
+}
