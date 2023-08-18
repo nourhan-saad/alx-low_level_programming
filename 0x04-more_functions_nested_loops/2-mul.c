655b9e881c1e17532f9794cd89ff56ec439e0ae1
nourhan-saad <nourhansaad707@gmail.com>
2023-08-17 18:28:14 -0700

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multublay the two intigir output
+ *
+ * @a: first input
+ *@b: secound input
+ *
+ * Return: the multublay of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
