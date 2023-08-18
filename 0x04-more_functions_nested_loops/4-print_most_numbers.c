2c3f3d9f137e2996b66599ec97c6d0f2b00331fd
nourhan-saad <nourhansaad707@gmail.com>
2023-08-17 19:00:59 -0700

    v

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers - print the number from 0 to 9
  *
- * Return: Always 0.
+ *return: 0 always sucsses
+ *
  */
 void print_most_numbers(void)
 {
