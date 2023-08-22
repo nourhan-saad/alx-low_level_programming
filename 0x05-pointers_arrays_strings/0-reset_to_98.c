d267ea886c67f2378c276127cabbc8ab6ac37626
nourhan-saad <nourhansaad707@gmail.com>
2023-08-21 19:00:20 -0700

    s

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *reset_to_98 - is use to change the parameter from
+ *		its number to 98
+ *
+ * @n: its a parameter
+ *
+ * retur: is nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
