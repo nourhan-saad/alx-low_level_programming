12c0443eaaecd3516fe87d1e6f0098c370abbcdf
nourhan-saad <nourhansaad707@gmail.com>
2023-08-21 19:33:25 -0700

    s

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - print the strig follwed by new line
+ *
+ * @str: its a parameter
+ *
+ * retur: is nothing
+*/
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*str + 0);
+		str++
+	}
+	_putchar('\n');
+}
